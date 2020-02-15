e52919c110cbd0b951d880f4ee6d88224b6ccfc3
Andrew Keller <Keller192@gmail.com>
2020-02-14 23:05:49 -0500

    refactors route handlers into controller files

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,83 +1,15 @@
-const fs = require('fs');
 const express = require('express');
-
+const tourController = require('./../controllers/tourController');
 const router = express.Router();
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    time: req.time,
-    data: {
-      tours
-    }
-  });
-};
-
-const getTour = (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  const tour = tours.find(tour => tour.id === id);
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour
-    }
-  });
-};
-
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour
-        }
-      });
-    }
-  );
-};
-
-const updateTour = (req, res) => {
-  // update logic here
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: 'patch placeholder'
-    }
-  });
-};
-
-const deleteTour = (req, res) => {
-  // delete logic here
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
-};
 
 router
   .route('/')
-  .get(getAllTours)
-  .post(createTour);
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
 router
   .route('/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
